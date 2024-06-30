bcf7274c82a3de5449d44e9166c18d6325ca5187
Edgar Sakwa <sakwaedgar57@gmail.com>
2024-06-30 16:29:54 +0300

    Update Models to Support JSON Serialization

--- app/models.py
+++ app/models.py
@@ -15,11 +15,30 @@ class QuizQuestion(db.Model):
     option4 = db.Column(db.String(100), nullable=False)
     correct_option = db.Column(db.String(100), nullable=False)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'question': self.question,
+            'option1': self.option1,
+            'option2': self.option2,
+            'option3': self.option3,
+            'option4': self.option4,
+            'correct_option': self.correct_option
+        }
+
 class QuizResult(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     score = db.Column(db.Integer, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'score': self.score,
+            'timestamp': self.timestamp.isoformat()
+        }
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
