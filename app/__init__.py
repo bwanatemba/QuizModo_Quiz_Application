69e26af2005d46cd5d1b5049b9efe5861b5ade9e
Edgar Sakwa <sakwaedgar57@gmail.com>
2024-06-30 17:03:39 +0300

    resolve a circular import issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,34 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from .models import User
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
     
-    db.init_app(app)
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    from .admin import admin as admin_blueprint
+    from .api import api_bp
+
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(api_bp, url_prefix='/api')
+
     login_manager.init_app(app)
     login_manager.login_view = 'auth.login'
-    
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-    
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
 
-    from .admin import admin as admin_blueprint
-    app.register_blueprint(admin_blueprint)
+    migrate.init_app(app, db)
 
-    from .api import api_bp
-    app.register_blueprint(api_bp, url_prefix='/api')
-    
+    db.init_app(app)
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
