c2189b4e409711db6a46c6e8a06fba55fb1c8ef8
Edgar Sakwa <sakwaedgar57@gmail.com>
2024-06-30 17:17:09 +0300

    resolve ValueError

--- app/auth.py
+++ app/auth.py
@@ -25,7 +25,7 @@ def signup():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        hashed_password = generate_password_hash(password, method='pbkdf2-sha256')
+        hashed_password = generate_password_hash(password, method='sha256')
         new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
