0e58b1be65b70cc4691c214b51a52e0476544f23
Edgar Sakwa <sakwaedgar57@gmail.com>
2024-06-30 16:37:03 +0300

    update models.py

--- app/models.py
+++ app/models.py
@@ -41,8 +41,3 @@ class QuizResult(db.Model):
             'timestamp': self.timestamp.isoformat()
         }
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
