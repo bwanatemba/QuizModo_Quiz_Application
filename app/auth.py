5037a83c06563309672e0449a77788b2f807e55b
Edgar Temba <sakwaedgar57@gmail.com>
2024-07-03 01:26:43 +0300

    resolve the unique constraint violation issue

--- app/auth.py
+++ app/auth.py
@@ -25,11 +25,23 @@ def signup():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('Username already exists. Please choose a different username.')
+            return redirect(url_for('auth.signup'))
+
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
         new_user = User(username=username, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        except sqlalchemy.exc.IntegrityError:
+            db.session.rollback()
+            flash('An error occurred while creating the account. Please try again.')
+            return redirect(url_for('auth.signup'))
+
     return render_template('signup.html')
 
 @auth.route('/logout')
